49158bf05eaf3994377a18780513650f5537fd36
Ben Phelps <phelps@MacBook Pro.(none)>
2012-09-02 11:50:36 -0500

    Add and allow username login.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829011958) do
+ActiveRecord::Schema.define(:version => 20120901154431) do
 
   create_table "armor_items", :force => true do |t|
     t.string   "armor_tier_id"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(:version => 20120829011958) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
