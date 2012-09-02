6d888be51aeaaf7aa0dccf65517c7c74f6400825
Ben Phelps <phelps@MacBook Pro.(none)>
2012-09-02 11:48:56 -0500

    Remove RailsAdmin route stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 TextMMMO::Application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-
   devise_for :users
   match '/home' => 'home#index'
   match '/home/what' => 'home#what'
